109d45b70677e6b416e03c80d30336aa6784c965
Dylan Perez <24607891+fearlessfreap24@users.noreply.github.com>
2021-12-01 22:48:19 -0600

    completeListItem() will add class name to div and remove complete button

--- js/todo.js
+++ js/todo.js
@@ -15,8 +15,16 @@ function addToDo(){
     
     todo_list_item_remove_btn = document.createElement("BUTTON")
     todo_list_item_remove_btn.innerText = "Remove"
+    todo_list_item_remove_btn.className = "remove_btn"
     todo_list_item_remove_btn.setAttribute("onclick", `removeListItem(${todo_count})`)
     todo_list_item_div.appendChild(todo_list_item_remove_btn)
+
+    todo_list_item_complete_btn = document.createElement("BUTTON")
+    todo_list_item_complete_btn.innerText = "Complete"
+    todo_list_item_complete_btn.id = `complete${todo_count}`
+    todo_list_item_complete_btn.className = "complete_btn"
+    todo_list_item_complete_btn.setAttribute("onclick", `completeListItem(${todo_count})`)
+    todo_list_item_div.appendChild(todo_list_item_complete_btn)
 
     todo_list.appendChild(todo_list_item_div)
     
@@ -26,7 +34,11 @@ function addToDo(){
 function removeListItem(item_number) {
     // console.log(`Remove item number ${item_number}`)
     document.getElementById(`todo_list_item_${item_number}`).remove()
-    
+}
+
+function completeListItem(item_number) {
+    document.getElementById(`todo_list_item_${item_number}`).className += " complete_list_item"
+    document.getElementById(`complete${item_number}`).remove()
 }
 
 
