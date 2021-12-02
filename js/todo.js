8aced508ed5ce7d50c167362b8a252ec86b61f40
Dylan Perez <24607891+fearlessfreap24@users.noreply.github.com>
2021-12-01 22:03:08 -0600

    added eventlistener to text box, reset text box after enter or button click

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,20 @@
-let todo_count = 1
-
 function addToDo(){
-    console.log("called addToDo")
+    let todo_text_box = document.getElementById("new_todo_text")
+    todo_list = document.getElementById("todo_list")
+    todo_text_value = todo_text_box.value
+    console.log(todo_text_value) 
+    todo_text_box.value = ""
+    todo_text_box.focus()
+    todo_text_box.select()
+    
+    
+    todo_count++
 }
+
+
+let todo_count = 1
+document.getElementById("new_todo_text").addEventListener("keypress", function (e) {
+    if ( e.code === "Enter" || e.code === "NumpadEnter"){
+        addToDo()
+    }
+})
