4bc2c1d889784ede16129d4ea2be704e592263cb
Dylan Perez <24607891+fearlessfreap24@users.noreply.github.com>
2021-12-01 22:23:31 -0600

    created removeListItem(), can now add list item to page with remove button

--- js/todo.js
+++ js/todo.js
@@ -2,13 +2,28 @@ function addToDo(){
     let todo_text_box = document.getElementById("new_todo_text")
     todo_list = document.getElementById("todo_list")
     todo_text_value = todo_text_box.value
-    console.log(todo_text_value) 
+    // console.log(todo_text_value) 
     todo_text_box.value = ""
     todo_text_box.focus()
     todo_text_box.select()
+    if (todo_text_value === "") { return }
+  
+    todo_list_item_div = document.createElement("div")
+    todo_list_item_div.id = `todo_list_item_${todo_count}`
+    todo_list_item_div.className = "list_item"
+    todo_list_item_div.innerText = todo_text_value
     
+    todo_list_item_remove_btn = document.createElement("BUTTON")
+    todo_list_item_remove_btn.innerText = "Remove"
+    todo_list_item_div.appendChild(todo_list_item_remove_btn)
+
+    todo_list.appendChild(todo_list_item_div)
     
     todo_count++
+}
+
+function removeListItem(item_number) {
+    console.log(`Remove item number ${item_number}`)
 }
 
 
